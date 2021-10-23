f78585fa098cf8206b08f6791740989f705a4bc4
nileshbe <92971515+nileshbe@users.noreply.github.com>
2021-10-23 13:42:49 +0530

    Update terraform1.tf

--- terraform1.tf
+++ terraform1.tf
@@ -2,7 +2,7 @@ provider "aws" {
  region = "us-east-1"
 }
 
-resource "aws_instance" "app-server1" {
+resource "aws_instance" "nilesh-server1" {
   ami           = "ami-02e136e904f3da870"
   instance_type = "t2.micro"
   key_name = "vodafone-devops"
